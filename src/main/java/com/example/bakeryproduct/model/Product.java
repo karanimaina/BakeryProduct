f2d17f9479a004e2586bda8076c1cb3a002f7f63
felixmaina <karani.maina2010@gmail.com>
2022-10-20 14:26:33 +0300

    add aGetter and a setter

--- src/main/java/com/example/bakeryproduct/model/Product.java
+++ src/main/java/com/example/bakeryproduct/model/Product.java
@@ -1,13 +1,17 @@
 package com.example.bakeryproduct.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Enumerated;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class Product {
     private long id;
     private String name;
