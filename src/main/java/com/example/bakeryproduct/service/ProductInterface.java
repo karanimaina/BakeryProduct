5c49aec8f1ddf6d88191d65758780a0dc653da23
felixmaina <karani.maina2010@gmail.com>
2022-10-20 20:00:09 +0300

    add a product dto

--- src/main/java/com/example/bakeryproduct/service/ProductInterface.java
+++ src/main/java/com/example/bakeryproduct/service/ProductInterface.java
@@ -1,6 +1,7 @@
 package com.example.bakeryproduct.service;
 
 import com.example.bakeryproduct.model.Product;
+import com.example.bakeryproduct.service.dto.ProductDto;
 
 public interface ProductInterface {
     Product addProduct(ProductDto productDto);
---
+++ src/main/java/com/example/bakeryproduct/service/dto/ProductDto.java
@@ -0,0 +1,5 @@
+package com.example.bakeryproduct.service.dto;
+
+public class ProductDto {
+
+}
