79e0c31ce75d972b7c82bfcf65a56a21588f5c1a
felixmaina <karani.maina2010@gmail.com>
2022-10-20 14:22:54 +0300

    annotate class with  @Entity

--- src/main/java/com/example/bakeryproduct/model/Product.java
+++ src/main/java/com/example/bakeryproduct/model/Product.java
@@ -2,13 +2,13 @@ package com.example.bakeryproduct.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Enumerated;
-
+@Entity
 public class Product {
     private long id;
     private String name;
     private String quantity;
     @Enumerated
-    private Type type
+    private Type type;
     private String price;
     private String Description;
     private String  amount;
