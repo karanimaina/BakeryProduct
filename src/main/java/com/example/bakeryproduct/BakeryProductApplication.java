1ba0f1fc42829805266622cf6dd153688e4fb223
felixmaina <karani.maina2010@gmail.com>
2022-10-20 14:09:14 +0300

    initialize the project dependencies

---
+++ src/main/java/com/example/bakeryproduct/BakeryProductApplication.java
@@ -0,0 +1,13 @@
+package com.example.bakeryproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeryProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakeryProductApplication.class, args);
+    }
+
+}
